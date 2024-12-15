e01306db2a954bbd6823d8f346228667c714889c
GRACENOBLE <gracenoble72@gmail.com>
2024-12-15 03:04:53 +0300

    feat(auth): finally did supabase auth 0

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
+"use client"
 import Hero from "@/components/hero";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
+import { Button } from "@/components/ui/button";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { createClient } from "@/utils/supabase/client";
 
-export default async function Index() {
+export default function Index() {
+  const supabase = createClient()
+  const handleClick = () => {
+    supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+  }
   return (
     <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
+      <Button onClick={handleClick}>Login with google</Button>
     </>
   );
 }
